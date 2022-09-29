018e87f15de65e73301f751d929af103af84786c
cristhian <christian.silva@bandtec.com.br>
2022-09-29 10:17:18 -0300

    correção de background na tela de login

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,9 @@
 window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+
+function entrar(){
+    window.location="../view/pages/public/login/login.html"
+
+  }
