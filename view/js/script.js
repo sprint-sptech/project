969f9fec25eaac04b47652e5394aed1e0d66852b
cristhian <christian.silva@bandtec.com.br>
2022-10-03 10:49:13 -0300

    alterações na dashboard conforme prototipo e botões linkados

--- view/js/script.js
+++ view/js/script.js
@@ -3,7 +3,25 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-function entrar(){
-    window.location="../view/pages/public/login/login.html"
+function login(){
+    window.location="./view/pages/public/login/login.html"
 
   }
+function criar_conta(){
+    window.location="../register/company/register.company.html"
+}
+function voltar_login(){
+    window.location="../../login/login.html"
+}
+function cadastrar_empresa(){
+    window.location="../user/register.user.html"
+}
+function cadastrar_usuario(){
+    window.location="../../login/login.html"
+}
+function entrar(){
+    window.location="../../private/dashboard/dashboard.html"
+}
+function sair(){
+    window.location="../../../../index.html"
+}
